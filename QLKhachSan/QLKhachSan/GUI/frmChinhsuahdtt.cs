b32b6c6e70942c9cfac73cb63c42ffbee784ba3e
a365ngay <35994656+a365ngay@users.noreply.github.com>
2018-04-09 21:12:17 +0700

    Update code thêm, sửa, xóa hóa đơn tt lần 2

--- QLKhachSan/QLKhachSan/GUI/frmChinhsuahdtt.cs
+++ QLKhachSan/QLKhachSan/GUI/frmChinhsuahdtt.cs
@@ -20,5 +20,87 @@ namespace QLKhachSan.GUI
 
             dataGridView1.DataSource = code.get_Hdtt();
         }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentRow.Cells["MaHDTT"].Value != null)
+            {
+                txtMahdtt.Text = dataGridView1.CurrentRow.Cells["MaHDTT"].Value.ToString();
+            }
+            else
+            {
+                txtMahdtt.Text = "";
+            }
+            if (dataGridView1.CurrentRow.Cells["MaPhong"].Value != null)
+            {
+                txtMap.Text = dataGridView1.CurrentRow.Cells["MaPhong"].Value.ToString();
+            }
+            else
+            {
+                txtMap.Text = "";
+            }
+            if (dataGridView1.CurrentRow.Cells["MaKHTT"].Value != null)
+            {
+                txtMakhtt.Text = dataGridView1.CurrentRow.Cells["MaKHTT"].Value.ToString();
+            }
+            else
+            {
+                txtMakhtt.Text = "";
+            }
+            if (dataGridView1.CurrentRow.Cells["NgayThanhToan"].Value != null)
+            {
+                dtpN.Text = dataGridView1.CurrentRow.Cells["NgayThanhToan"].Value.ToString();
+            }
+            else
+            {
+                dtpN.Text = "";
+            }
+            if (dataGridView1.CurrentRow.Cells["TongTienThanhToan"].Value != null)
+            {
+                txtTien.Text = dataGridView1.CurrentRow.Cells["TongTienThanhToan"].Value.ToString();
+            }
+            else
+            {
+                txtTien.Text = "";
+            }
+            if (dataGridView1.CurrentRow.Cells["MaNV"].Value != null)
+            {
+                txtManv.Text = dataGridView1.CurrentRow.Cells["MaNV"].Value.ToString();
+            }
+            else
+            {
+                txtManv.Text = "";
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            frmThemhdtt themkh = null;
+            Check_Them:
+            if (themkh == null || themkh.IsDisposed)
+            {
+                themkh = new frmThemhdtt();
+            }
+            if (themkh.ShowDialog() == DialogResult.OK)
+            {
+                if (frmThemhdtt.x == 0)
+                {
+                    goto Check_Them;
+                }
+            }
+            dataGridView1.DataSource = code.get_Hdtt();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            code.edit_Hdtt(txtMahdtt.Text, txtMap.Text, txtMakhtt.Text, dtpN.Text, txtTien.Text, txtManv.Text);
+            dataGridView1.DataSource = code.get_Hdtt();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            code.delete_Hdtt(txtMahdtt.Text);
+            dataGridView1.DataSource = code.get_Hdtt();
+        }
     }
 }
